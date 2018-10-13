35accb9d2beaadceb82306b45573cf7fb289ffe2
dasdachs <jani.sumak@gmail.com>
2018-10-13 16:34:59 +0200

    Removed robots.txt and added robots meta tag to layout.html

--- docs/conf.py
+++ docs/conf.py
@@ -241,8 +241,3 @@ except ImportError:
                  'to this.')
 
 linkcheck_anchors = False
-
-# Add any extra paths that contain custom files (such as robots.txt or
-# .htaccess) here, relative to this directory. These files are copied
-# directly to the root of the documentation.
-html_extra_path = ['robots.txt']
